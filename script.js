abd625b734a01be78ba82263ab003c2431457a7d
kadiem <kadiem1999@gmail.com>
2023-12-18 15:49:54 +0000

    dsfjkhhge

--- script.js
+++ script.js
@@ -55,3 +55,37 @@ circles.forEach(elem=>{
         pointsMarked[i].classList.add('marked')
     }
 })
+var mixer = mixitup('.portofolio-gallery');
+
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",this.window.scrollY > 50)
+})
+
+
+
+
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
